b62161347e747485de35dddcfccd8812aaa2b4fe
Admin Server <apostlma@ucsd.edu>
2022-07-13 13:46:42 +0000

    users.js activity - solutions

--- routes/users.js
+++ routes/users.js
@@ -8,26 +8,55 @@ userRouter.use(express.json())
 
 // READ ALL USERS W/ PROPER PRIVILEGES
 userRouter.get('/all', (req,res) => {
+    console.log('admin header', req.headers.admin)
+    if(req.headers.admin){
+        queryStatement = " select * from users;"
+        databaseCall(queryStatement, req, res)
+    }
+    else {
+        res.status(403).json({ "forbidden":"PLease log in to continue"})
+        // console.log(" header 'admin' is not true  - not authorized")
+    }
    
 })
 
 // READ USER WITH USERID
-carRouter.get('/:userId', (req,res) => {
+userRouter.get('/:userId', (req,res) => {
+    queryStatement = `SELECT * FROM users WHERE user_id = '${req.params.userId}';`
+    console.log(queryStatement)
+    databaseCall(queryStatement, req, res)
    
 })
 
 // DELETE USER WITH USERID
 userRouter.delete('/:userId', (req,res) => {
+    queryStatement = `DELETE FROM users WHERE user_id = '${req.params.userId}' RETURNING *;`
+    console.log(queryStatement)
+    databaseCall(queryStatement, req, res)
+
     
 })
 
 // CREATE USER
 userRouter.post('/new', (req,res) =>{
+    console.log(req.body)
+    email = req.body.email 
+    phone = req.body.phone
+    queryStatement = `INSERT INTO users (name,email, phone) VALUES ('${ req.body.name}','${email}',${phone} ) RETURNING *;`
+    databaseCall(queryStatement, req, res)
  
 })
 
 // UDPATE USER WITH USERID
 userRouter.put('/update', (req,res) =>{
+    console.log(req.body)
+    userId = req.body.userId
+    email = req.body.email 
+    phone = req.body.phone
+    queryStatement = `UPDATE users SET name = '${ req.body.name}', email = '${email}', phone = ${phone} WHERE user_id = '${userId}' RETURNING *;`
+    console.log(queryStatement)
+    databaseCall(queryStatement, req, res)
+
    
 })
 
