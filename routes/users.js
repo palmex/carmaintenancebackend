4ecb9469f9dc8739e07771d4c32164eb4a5d9e43
Admin Server <apostlma@ucsd.edu>
2022-07-13 12:52:44 +0000

    users route activity

---
+++ routes/users.js
@@ -0,0 +1,58 @@
+const express = require('express')
+const db = require('../db')
+var userRouter = express.Router()
+
+userRouter.use(express.json())
+// ------- functional ------------
+// this is a full CRUD (create,read,update, delete) route for our users table in our database
+
+// READ ALL USERS W/ PROPER PRIVILEGES
+userRouter.get('/all', (req,res) => {
+   
+})
+
+// READ USER WITH CARID
+carRouter.get('/:userId', (req,res) => {
+   
+})
+
+// DELETE USER WITH CARID
+userRouter.delete('/:userId', (req,res) => {
+    
+})
+
+// CREATE USER
+userRouter.post('/new', (req,res) =>{
+ 
+})
+
+// UDPATE USER WITH USERID
+userRouter.put('/update', (req,res) =>{
+   
+})
+
+// using a function to call database instead of having this 
+// db.query(queryStatement, (error, results) =>{
+//     if (error){
+//         res.status(500).json(error) 
+//     } else
+//     {res.status(200).json(results.rows)}
+    
+// } )
+// in every endpoint
+
+const databaseCall = (queryStatement, request, res) => {
+    db.query(queryStatement, (error, results) =>{
+        if (error){
+            res.status(500).json(error) 
+        } else
+        {res.status(200).json(results.rows)}
+        
+    } )
+}
+
+
+
+
+// ------- ---------- ------------
+module.exports = userRouter;
